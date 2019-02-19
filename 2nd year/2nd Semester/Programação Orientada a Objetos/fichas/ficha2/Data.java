d5704ce413ed9fecd87ee8ee564e4fc2ec663f2f
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-02-19 23:13:32 +0000

    [Programação Orientada aos Objetos]: Alguns exercícios da ficha 2

--- 2nd year/2nd Semester/Programação Orientada a Objetos/fichas/ficha2/Data.java
+++ 2nd year/2nd Semester/Programação Orientada a Objetos/fichas/ficha2/Data.java
@@ -2,10 +2,4 @@ import java.util.LocalDate;
 import java.util.Arrays;
 
 public class Data {
-
-    public void insereData(LocalDate data, LocalData[] arr) {
-        int length = arr.length;
-        LocalDate[] holder = new LocalDate[length + 1];
-        System.arraycopy(data, 0, holder, length)
-    }
 }
--- 2nd year/2nd Semester/Programação Orientada a Objetos/fichas/ficha2/Ficha2.java
+++ 2nd year/2nd Semester/Programação Orientada a Objetos/fichas/ficha2/Ficha2.java
@@ -5,23 +5,11 @@ public class Ficha2 {
 
     // Exercício 1
     // a
-    static void lerArray() {
-        int ceiling = 100;
-        int input = 1;
-        int i = 0;
-        int[] list = new int[ceiling];
-        while(input!= 0) {
-            System.out.println("Introduz o número que queres no índice " + i + " do array.");
-            Scanner scan = new Scanner(System.in);
-            input = scan.nextInt();
-            list[i] = input;
-            i++;
-        }
-        // PRINT ARRAY
-        for(int j = 0; j < i; j++) {
-            System.out.println("O indíce número " + j + "tem o valor de: " + list[j]);
-        }
-    }
+    public int min(int[] arr) {
+        int min = 10000;
+        for(int d : arr) if(d < min) min = d;
+        return min;
+    } 
 
     // b
     public int[] arrayEntre(int[] array, int i, int f) {
@@ -31,5 +19,92 @@ public class Ficha2 {
         return res;
     }
 
+    //c
+    public int[] comuns(int[] arr1, int[] arr2) {
+        int length = (arr.length > arr2.length) ? arr.length : arr2.length;
+        int[] newArr = new int[length];
+        int index = 0;
+        for(int i = 0; i < arr.length; i++)
+            for(int j = 0; j < arr2.length; j++)
+                if(arr2[j] == arr[i]) newArr[index++] = arr[i]; index++;
+        return newArray;
+    }
+
     // Exercício 2
+    //b
+    public int somaNotasUc(int[][] notasTurma, int uc) {
+        int sum = 0;
+        for(int i = 0; i < 5; i++)
+            sum += notasTurma[i][uc];
+        return sum;
+    }
+
+    //c
+    public float meanAluno(int[][] notasTurma, int aluno) {
+        int sum = 0;
+        float mean;
+        for(int i = 0; i < 5; i++)
+            sum += notasTurma[aluno][i];
+        mean = sum / 5;
+        return mean; 
+    }
+
+    //d
+    public float meanUC(int[][] notasTurma, int uc) {
+        int sum = somaNotasUc(notasTurma, uc);
+        float mean = sum / 5;
+        return mean;
+    }
+
+    //e
+    public int highestGrade(int[][] notasTurma) {
+        int max = -1;
+        for(int i = 0; i < 5; i++)
+            for(int j = 0; j < 5; j++)
+                if(notasTurma[i][j] > max) max = notasTurma[i][j];
+        return max;
+    }
+
+    //f
+    public int lowestGrade(int[][] notasTurma) {
+        int min = 21;
+        for(int i = 0; i < 5; i++)
+            for(int j = 0; j < 5; j++)
+                if(notasTurma[i][j] < min) min = notasTurma[i][j];
+        return min;
+    }
+
+    //g
+    public int[] gradesAboveArgument(int[][] notasTurma, int argument) {
+        int maxPossibleLength = 5 * 5;
+        int[] grades = new int[maxPossibleLength];
+        int index = 0;
+        for(int i = 0; i < 5; i++)
+            for(int j = 0; j < 5; j++)
+                if(notasTurma[i][j] > argument) grades[index++] = notasTurma[i][j];
+        System.arraycopy(grades, 0, grades, 0, index);
+        return grades;
+    }
+
+    //h
+    public String grades(int[][] notasTurma) {
+        String outp = "";
+        for(int i = 0; i < 5; i++)
+            for(int j = 0; j < 5; j++) {
+                outp = outp + i + "-> " + j + "; ";
+            }
+        outp += "\n";
+    }
+
+    //i
+    public int highestMeanUC(int[][] notasTurma) {
+        float currentMean;
+        float highestMean = 0;
+        int index = -1;
+        for(int i = 0; i < 5; i++) {
+            currentMean = meanUC(notasTurma, i);
+            if(currentMean > highestMean) {highestMean = currentMean; index = i;}
+        }
+        return index;
+    }
 }
