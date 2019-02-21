642851798ede80bcda3e89e5c4517e3ab578dfe9
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-02-21 10:39:27 +0000

    [Programação Orientada aos Objetos]: Exercício 6 -> Ficha 2

--- 2nd year/2nd Semester/Programação Orientada a Objetos/fichas/ficha2/Ficha2.java
+++ 2nd year/2nd Semester/Programação Orientada a Objetos/fichas/ficha2/Ficha2.java
@@ -119,4 +119,43 @@ public class Ficha2 {
     public int binarySearch(int[] lista, int goal) {
         return(Arrays.binarySearch(lista, goal));
     }
+
+    // Exercício 6
+    // a
+    public void readMatrix(int[][] matrix) {
+        int holder;
+        //vou só percorrer a matriz, a pergunta não faz muito sentido
+        for(int i = 0; i < matrix.length; i++)
+            for(int j = 0; j < matrix[0].length; j++)
+                holder = matrix[i][j];
+    }
+
+    //b
+    // Vou assumir que as matrizes têm o mesmo tamanho
+    public int[][] matrixSum(int[][] m1, int[][] m2) {
+        int[][] sum = new int[m1.length][m1[0].length];
+        for(int i = 0; i < m1.length; i++)
+            for(int j = 0; j < m1[0].length; j++)
+                sum[i][j] = m1[i][j] + m2[i][j];
+        return sum;
+    }
+
+    //c
+    public boolean equal(int[][] m1, int[][] m2) {
+        boolean r = true;
+        for(int i = 0; i < m1.length; i++)
+            for(int j = 0; j < m1[0].length; j++)
+                if(m1[i][j] != m2[i][j]) r = false;
+        return r;
+    }
+
+    //d
+    public int[][] oposta(int[][] matrix) {
+        int[][] oposta = new int[matrix.length][matrix[0].length];
+        for(int i = 0; i < matrix.length; i++)
+            for(int j = 0; j < matrix[0].length; j++)
+                oposta[i][j] = - matrix[i][j];
+        return oposta;
+    }
 }
+
