9cf623f0c50449df3aa55045969b1226efa962a8
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-02-22 21:45:49 +0000

    [Sistemas Operativos]: Exercício 6 -> Guião 1

--- 2nd year/2nd Semester/Sistemas Operativos/guioes/guide1/main.c
+++ 2nd year/2nd Semester/Sistemas Operativos/guioes/guide1/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFFERSIZE 1024
-
+#define TENMiB     1024 * 1024 * 10
 /* Exercício 1 */
 void populateBuffer(char *string) {
     for(int i = 0; i < 1024; i++) {
@@ -18,7 +17,7 @@ int tenMb(char *nome) {
     char buffer[BUFFERSIZE];
     int fp = open(nome, O_CREAT | O_TRUNC | O_WRONLY, 0777);
     populateBuffer(buffer);
-    for(int i = 0; i < 1024*10; i++) {
+    for(int i = 0; i < (TENMiB / BUFFERSIZE); i++) {
         write(fp, buffer, 1024);
     }
 }
@@ -28,7 +27,9 @@ void myCat() {
     char byte;
     int r;
     do {
+        // 0 -> STDIN_FILENO
         r = read(0, &byte, 1);
+        // 1 -> SDTOUT_FILENO
         write(1, &byte, 1);
     }
     while(r != 0);
@@ -68,7 +69,24 @@ ssize_t readln(int fildes, void *buf, size_t nbyte) {
 }
 
 /* Exercício 6 */
+/* Consideramos um tamanho de buffer aceitável para ler uma linha 1024 */
+void nl(char *filename) {
+    int lineCounter = 1;
+    int n;
+    int fp = open(filename, O_CREAT | O_RDONLY);
+    char buf[1024];
+    while((n = readln(fp, buf, 1024)) > 0) {
+        char lineNum[12];
+        sprintf(lineNum, "%6d ", lineCounter);
+        write(1, lineNum, strlen(lineNum));
+        write(1, buf, n);
+        if(buf[n-1] == '\n') lineCounter++;
+    }
+}
+
+
 /* Main */
 int main(int argc, char **argv) {
-    myCatBytesVariable(atoi(argv[1]));
+    nl(argv[1]);
+    return 0;
 }
