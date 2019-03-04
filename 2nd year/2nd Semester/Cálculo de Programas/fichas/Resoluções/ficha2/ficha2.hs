b725a3ef0c4d281db5beff793c36c1f8f03bdf62
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-03-04 22:42:58 +0000

    [Cálculo de Programas]: Corrige exercício 4 da ficha 2

--- 2nd year/2nd Semester/Cálculo de Programas/fichas/Resoluções/ficha2/ficha2.hs
+++ 2nd year/2nd Semester/Cálculo de Programas/fichas/Resoluções/ficha2/ficha2.hs
@@ -1,12 +1,18 @@
 module Ficha2 where
 import CP
 
--- Exercício 4 --
-xor :: Bool -> Bool -> Bool
-xor p1 p2 = p1 /= p2
+-- -- Exercício 4 --
+-- xor :: Bool -> Bool -> Bool
+-- xor p1 p2 = p1 /= p2
+
+-- f :: ((Bool, Bool), Bool) -> Bool
+-- f = (uncurry xor) . (uncurry (&&) >< id)
+
+xor :: (Bool, Bool) -> Bool
+xor (b1, b2) = b1 /= b2
 
 f :: ((Bool, Bool), Bool) -> Bool
-f = (uncurry xor) . (uncurry (&&) >< id)
+f = xor . (and >< id)
 
 -- Não pergunta mas é relativamente fácil definir a função g
 g :: ((Bool, Bool), Bool) -> ((Bool, Bool), Bool)
