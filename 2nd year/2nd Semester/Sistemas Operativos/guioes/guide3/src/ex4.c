fd87c6dc3bc38d8676775ca897024b68f1b31bde
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-03-18 22:54:09 +0000

    [Sistemas Operativos]: Adiciona resolução Guião 3 até ao exercício 5

---
+++ 2nd year/2nd Semester/Sistemas Operativos/guioes/guide3/src/ex4.c
@@ -0,0 +1,5 @@
+#include "guide3.h"
+
+int main(int argc, char **argv) {
+    execv("./ex3", argv);
+}
--- 2nd year/2nd Semester/Sistemas Operativos/guioes/guide3/src/ex5.c
+++ 2nd year/2nd Semester/Sistemas Operativos/guioes/guide3/src/ex5.c
@@ -1,5 +1,8 @@
 #include "guide3.h"
 
 int main(int argc, char **argv) {
-
+    for(int i = 1; i < argc; i++) {
+        if(!fork())
+            execlp(argv[i], argv[i], NULL);
+    }
 }
