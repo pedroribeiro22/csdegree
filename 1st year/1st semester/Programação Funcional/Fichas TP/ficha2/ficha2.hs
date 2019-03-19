85ecf98c9f2f6adaea3e4d8bdcfdfb1810a8752e
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-03-19 13:08:56 +0000

    [Programação Funcional]: Conclusão Ficha 2

--- 1st year/1st semester/Programação Funcional/Fichas TP/ficha2/ficha2.hs
+++ 1st year/1st semester/Programação Funcional/Fichas TP/ficha2/ficha2.hs
@@ -1,5 +1,6 @@
 module Ficha2 where
 import Data.Char
+import Data.List
 
 -- Exercício 2 --
 -- a
@@ -52,3 +53,31 @@ nosPrimeiros :: (Eq a) => a -> [(a, b)] -> Bool
 nosPrimeiros a l = elem a (map fst l)
 
 -- c
+minFst :: (Ord a) => [(a, b)] -> a
+minFst l = minimum $ map fst l
+
+-- d
+sndMinFst :: (Ord a) => [(a, b)] -> b
+sndMinFst l@(x:xs) = if a == fst x then snd x
+                   else sndMinFst xs
+            where a = minFst l
+
+-- e
+get1st :: (Num a, Num b, Num c) => (a, b, c) -> a
+get1st (x, y, z) = x
+
+get2nd :: (Num a, Num b, Num c) => (a, b, c) -> b
+get2nd (x, y, z) = y
+
+get3rd :: (Num a, Num b, Num c) => (a, b, c) -> c
+get3rd (x, y, z) = z
+
+sumTriplos :: (Num a, Num b, Num c) => [(a, b, c)] -> (a, b, c)
+sumTriplos l = (sum $ map get1st l, sum $ map get2nd l, sum $ map get3rd l)
+
+-- f
+sumTriplo :: (Ord a, Num a) => (a, a, a) -> a
+sumTriplo (x, y, z) = x + y + z
+
+maxTriplo :: (Ord a, Num a) => [(a, a, a)] -> a
+maxTriplo l = maximum $ map sumTriplo l
