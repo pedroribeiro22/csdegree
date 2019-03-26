15d5a4c88caeb119d35b5a17a0d2e97ae0b1b4f3
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-03-26 15:45:36 +0000

    [Sistemas Operativos]: Exercício 1 -> Guião 5

---
+++ 2nd year/2nd Semester/Sistemas Operativos/guioes/guide5/src/ex1.c
@@ -0,0 +1,57 @@
+#include "guide5.h"
+
+int filho2pai() {
+    int fds[2];
+    int status;
+    char texto[] = "És um boi!";
+    char leitura[1024];
+    if(pipe(fds) == -1) return -1;
+    switch(fork()) {
+        case -1:
+            perror("Fodeu fork\n");
+        case 0:
+            close(fds[0]);
+            write(fds[1], &texto, sizeof(texto));
+            printf("[FILHO]: Wrote line to pipe\n");
+            close(fds[1]);
+            _exit(0);
+        default:
+            close(fds[1]);
+            read(fds[0], &leitura, sizeof(texto));
+            printf("[PAI]: read %s from pipe\n", leitura);
+            close(fds[0]);
+            wait(&status);
+    }
+    return 0;
+}
+
+int pai2filho() {
+    int fds[2];
+    int status;
+    char texto[] = "És um boi!";
+    char leitura[1024];
+    if(pipe(fds) == -1) return -1;
+    switch(fork()) {
+        case -1:
+            perror("Fodeu fork\n");
+        case 0:
+            close(fds[1]);
+            read(fds[0], &leitura, sizeof(leitura));
+            printf("[FILHO]: read %s from pipe\n", leitura);
+            close(fds[0]);
+            _exit(0);
+        default:
+            close(fds[0]);
+            write(fds[1], &texto, sizeof(texto));
+            printf("[PAI]: Wrote line to pipe\n");
+            close(fds[1]);
+            wait(&status);
+    }
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int r = filho2pai();
+    printf("%d\n", r);
+    return 0;
+}
---
+++ 2nd year/2nd Semester/Sistemas Operativos/guioes/guide5/src/guide5.h
@@ -0,0 +1,10 @@
+#ifndef _GUIDE_5_H
+#define _GUIDE_5_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#endif
