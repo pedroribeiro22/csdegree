90c1ba1368cd38d95c8a73fd132c277dc6ae7137
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-06-11 15:56:08 +0100

    [Sistemas Operativos]: Guião 2

--- 2nd year/2nd Semester/Sistemas Operativos/guioes/guide2/src/ex4.c
+++ 2nd year/2nd Semester/Sistemas Operativos/guioes/guide2/src/ex4.c
@@ -1,16 +1,17 @@
-#include "guide2.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
     for(int i = 1; i < 11; i++) {
-        if(!fork())
-            // possível código que queiramos executar
+        if(fork() == 0)
             _exit(i);
     }
-    pid_t pid;
-    int status;
-    while((pid = wait(&status)) != -1)
-        printf("%d morreu com o código de saída: %d\n", pid, WEXITSTATUS(status));
-    return 0;
 
+    int pid, status;
+    while((pid = wait(&status)) != -1) {
+        printf("%d terminou\n", pid);
+    }
 }
--- 2nd year/2nd Semester/Sistemas Operativos/guioes/guide2/src/ex6.c
+++ 2nd year/2nd Semester/Sistemas Operativos/guioes/guide2/src/ex6.c
@@ -1,41 +1,20 @@
-#include "guide2.h"
-#define COLUNAS 100
-#define LINHAS 10
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-void povoaMatriz(int matriz[10][100]) {
-    int ground = 0;
-    int ceiling = 10;
-    srand(time(NULL));
-    for(int i = 0; i < LINHAS; i++)
-        for(int j = 0; j < COLUNAS; j++)
-            matriz[i][j] = rand() % (ceiling + 1 - ground) + ground;
+void povoaMatriz(void **matriz, int rows, int columns) {
+
+    int **myMatriz = (int **) matriz;
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++) {
+            srand(time(NULL));
+            myMatriz[i][j] = rand() % 10;
+        }
 }
 
 int main(int argc, char **argv) {
-    int alvo;
-    int matrix[LINHAS][COLUNAS];
-    alvo = atoi(argv[1]);
-    povoaMatriz(matrix);
-    // podemos ir guardando o `processID` dos filhos num array
-    int childs[LINHAS];
-    for(int i = 0; i < LINHAS; i++) {
-        if(!(childs[i] = fork())) {
-            for(int j = 0; j < COLUNAS; j++) {
-                if(matrix[i][j] == alvo) _exit(1);
-            }
-            _exit(0);
-        }
-    }
-    pid_t pid;
-    int status;
-    while((pid = wait(&status)) != -1) {
-        int i;
-        // temos de fazer um pequeno ciclo para descobrir a linha do processo que acabou
-        for(i = 0; i < LINHAS && pid != childs[i]; i++);
-        // temos agora a linha do processo que acabou
-        // vamos descobrir que valor retornou ao pai quando acabou
-        // caso tenha retornado `1` encontrou o `alvo`, caso contrário retornou `0`
-        if(WEXITSTATUS(status)) printf("O processo %d encontrou o alvo na linha %d\n", pid, i);
-    }
-    return 0;
+
+    int matriz[20][50];
+    povoaMatriz(matriz, 20, 50);
 }
