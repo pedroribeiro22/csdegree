abd416491e21ebc033d4d0a640c2f95db5d06474
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2019-06-13 15:18:22 +0100

    :art: New grupo3.c

--- 2nd year/2nd Semester/Sistemas Operativos/testes/teste1718/grupo3.c
+++ 2nd year/2nd Semester/Sistemas Operativos/testes/teste1718/grupo3.c
@@ -1,23 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <sys/types.h>
 
+int getDigito(char *s) {
+    char num[4096];
+    int i = 0;
+    while(s[i] != ' ') {
+        num[i] = s[i];
+        i++;
+    }
+    num[i] = '\0';
+    return atoi(num);
+}
+
 int main(int argc, char **argv) {
 
     if(argc < 3) return -1;
 
-    // Número de produtores
     int p = atoi(argv[1]);
-
-    // Número de consumidores
     int c = atoi(argv[2]);
 
     int pipeProdutores[2];
     pipe(pipeProdutores);
 
     for(int i = 0; i < p; i++) {
-
         if(fork() == 0) {
             close(pipeProdutores[0]);
             dup2(pipeProdutores[1], 1);
@@ -31,14 +40,12 @@ int main(int argc, char **argv) {
     int pipeConsumidores[c][2];
 
     for(int i = 0; i < c; i++) {
-
         pipe(pipeConsumidores[i]);
         if(fork() == 0) {
-            for(int p = 0; p < i; p++) {
-                close(pipeConsumidores[p][0]);
-                close(pipeConsumidores[p][1]);
+            for(int j = 0; j < i; i++) {
+                close(pipeConsumidores[j][0]);
+                close(pipeConsumidores[j][1]);
             }
-
             close(pipeConsumidores[i][1]);
             dup2(pipeConsumidores[i][0], 0);
             close(pipeConsumidores[i][0]);
@@ -47,4 +54,17 @@ int main(int argc, char **argv) {
         }
         else close(pipeConsumidores[i][0]);
     }
+
+    char buf[4096];
+    int size;
+
+    while(size = readln(pipeProdutores[0], buf, 4096) > 0) {
+        int consumidor = getDigito(buf);
+        write(pipeConsumidores[c][1], buf + sizeof(int), size - sizeof(int));
+    }
+
+    for(int i = 0; i < c; i++)
+        close(pipeConsumidores[i][1]);
+    close(pipeProdutores[0]);
+    return 0;
 }
