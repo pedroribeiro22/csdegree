4c733b263d750e2c1ae5013bdf826b15a276a651
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-02-12 22:11:19 +0000

    PL exercises

---
+++ 3rd year/2nd Semester/CG/aula1/main.cpp
@@ -0,0 +1,106 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
+
+
+float teapot_size = 0.7;
+float eyeX = 0.0;
+float eyeY = 0.0;
+float eyeZ = 5.0;
+float angle = 0;
+
+void changeSize(int w, int h) {
+
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if(h == 0)
+		h = 1;
+
+	// compute window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load Identity Matrix
+	glLoadIdentity();
+	
+	// Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+	// Set perspective
+	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void keyPress(unsigned char key, int x, int y) {
+    if(key == 'a') {
+        angle -= 0.1;
+        eyeX -= sin(angle);
+        eyeZ -= cos(angle);
+    }
+    if(key == 'd') {
+        angle += 0.1;
+        eyeX += sin(angle);
+        eyeZ += cos(angle);
+    }
+}
+
+
+void renderScene(void) {
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+	glLoadIdentity();
+	gluLookAt(eyeX, eyeY, eyeZ,
+		      0.0,0.0,0.0,
+			  0.0f,1.0f,0.0f);
+
+
+    glutWireTeapot(teapot_size);
+
+
+
+	// End of frame
+	glutSwapBuffers();
+
+}
+
+
+
+
+int main(int argc, char **argv) {
+
+    glutInit(&argc, argv);
+    glutInitWindowPosition( 37, 42);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("Test");
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+    glutKeyboardFunc(keyPress);
+    glutIdleFunc(renderScene);
+
+
+
+// OpenGL settings 
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glClearColor(0.0f,0.0f,0.0f,0.0f);
+
+
+
+// enter GLUT's main loop
+	glutMainLoop();
+	
+	return 1;
+}
+
--- 3rd year/2nd Semester/PL/aula1/binary_string.php
+++ 3rd year/2nd Semester/PL/aula1/binary_string.php
@@ -1,6 +1,7 @@
 <?php
 
 $pattern = "/0*1((0*1){2})*0+/";
+$correct_pattern = "/(00|11|(01|10)(00|11)*(10|01))*(1|(01|10)(11|00)*0)/";
 $another_pattern = "/(0*0*1)((0*1){2})*0*0*/";
 $text_array = array("10101", "111110", "1110011");
 
@@ -13,7 +14,7 @@ foreach ($text_array as $linha) {
          else echo " -> ÍMPAR\n";
     }
 
-    if(preg_match($another_pattern, $linha)) {
+    if(preg_match($correct_pattern, $linha)) {
 
         echo "Match found in: $linha\n";
 
