576c1a0e7d4997e1857fc837fe58c89809c4e044
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-05-24 00:14:08 +0100

    idk

--- 3rd year/2nd Semester/SRCR/ficha13/ficha13.R
+++ 3rd year/2nd Semester/SRCR/ficha13/ficha13.R
@@ -1,17 +1,21 @@
+# Ficha 13
+
+# instalar pacote "neuralnet" caso ainda n?o esteja instalado
+# install.packages(neuralnet)
 library(neuralnet)
 
 setwd("/home/pedro/Code/csdegree/3rd year/2nd Semester/SRCR/ficha13")
 
 # ler dataset de treino de um CSV, separado por "," e casa decimal representada por "."
-trainset <- read.csv("ficha13 normalizado.csv",header=TRUE,sep=",",dec=".")
+trainset <- read.csv("trainset.csv",header=TRUE,sep=",",dec=".")
 
 head(trainset)
 
 
-# definição da formula de treino
+# defini??o da formula de treino
 formulaRNA <- Avaliacao ~ Vencimento+Habitacao+Automovel+Cartao
 
-# (1) treinar rede neuronal chamada "creditnet" com 5 nodos intermédios
+# (1) treinar rede neuronal chamada "creditnet" com 5 nodos interm?dios
 creditnet <- neuralnet(formulaRNA, trainset, hidden = c(5), threshold = 0.1)
 
 # testar a RNA com os casos usados para treino
@@ -20,7 +24,7 @@ creditnet.results <- compute(creditnet, teste)
 print(round(creditnet.results$net.result,digits=0))
 
 
-# (2) treinar rede neuronal chamada "creditnet" com 6,4,2 nodos intermédios
+# (2) treinar rede neuronal chamada "creditnet" com 6,4,2 nodos interm?dios
 creditnet <- neuralnet(formulaRNA, trainset, hidden = c(6,4,2), lifesign = "full", threshold = 0.01)
 
 
@@ -43,7 +47,7 @@ test[2,] <- data.frame(Vencimento=0.7,Habitacao=0.4,Automovel=0.55,Cartao=0.1)
 # testar novos casos na rede neuronal
 creditnet.results <- compute(creditnet, test)
 
-# imprimir o resultado final (uso da função round como auxiliar para arredondar o resultado final)
+# imprimir o resultado final (uso da fun??o round como auxiliar para arredondar o resultado final)
 print(round(creditnet.results$net.result))
 print(round(creditnet.results$net.result,digits=1))
 
@@ -51,3 +55,10 @@ print(round(creditnet.results$net.result,digits=1))
 teste <- subset(trainset, select = c("Vencimento","Habitacao","Automovel","Cartao"))
 creditnet.results <- compute(creditnet, teste)
 print(round(creditnet.results$net.result,digits=0))
+
+# testar novos casos na rede neuronal
+creditnet.results <- compute(creditnet, test)
+
+# imprimir o resultado final (uso da fun??o round como auxiliar para arredondar o resultado final)
+print(round(creditnet.results$net.result))
+print(round(creditnet.results$net.result,digits=1))
