2afe8134c524dcaa89bf877f3e919d57f64b0420
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-10-25 00:47:02 +0100

    Finshed PSD homework

--- 4rd year/PSD/guide1/priority_queue.erl
+++ 4rd year/PSD/guide1/priority_queue.erl
@@ -1,45 +1,71 @@
 -module(priority_queue).
--export([my_create/0, my_enqueue/3, my_dequeue/1, trees_my_create/2, trees_my_enqueue/3, trees_my_dequeue/2]).
+-export([my_create/0, my_enqueue/3, my_dequeue/1, tree_create/0, tree_enqueue/3, tree_dequeue/1]).
 -import(myqueue, [create/1, enqueue/2, dequeue/1]).
--import(gb_trees, [tree/2, get/2, update/3]).
 
 
-% É mais fácil assumir que o número de prioridades era conhecido à priori
-% Vamos assumir neste caso que o número de prioridades é 3
-
-% --- a) ---
 
 my_create() -> {[], [], []}.
 
-my_enqueue({Priority_1, Priority_2, Priority_3}, Elem, 1) -> {myqueue:enqueue(Priority_1, Elem), Priority_2, Priority_3};
-my_enqueue({Priority_1, Priority_2, Priority_3}, Elem, 2) -> {Priority_1, myqueue:enqueue(Priority_2, Elem), Priority_3};
-my_enqueue({Priority_1, Priority_2, Priority_3}, Elem, 3) -> {Priority_1, Priority_2, myqueue:enqueue(Priority_3, Elem)}.
+my_enqueue({Priority_1, Priority_2, Priority_3}, Elem, 1) -> {enqueue(Priority_1, Elem), Priority_2, Priority_3};
+my_enqueue({Priority_1, Priority_2, Priority_3}, Elem, 2) -> {Priority_1, enqueue(Priority_2, Elem), Priority_3};
+my_enqueue({Priority_1, Priority_2, Priority_3}, Elem, 3) -> {Priority_1, Priority_2, enqueue(Priority_3, Elem)}.
 
 my_dequeue({[], [], []}) -> empty;
 my_dequeue({[], Priority_2, Priority_3}) ->
-    {Remainder, Element} = myqueue:dequeue(Priority_2),
+    {Remainder, Element} = dequeue(Priority_2),
     {{[], Remainder, Priority_3}, Element};
 my_dequeue({[], [], Priority_3}) ->
-    {Remainder, Element} = myqueue:dequeue(Priority_3),
+    {Remainder, Element} = dequeue(Priority_3),
     {{[], [], Remainder}, Element};
 my_dequeue({Priority_1, Priority_2, Priority_3}) ->
-    {Remainder, Element} = myqueue:dequeue(Priority_1),
+    {Remainder, Element} = dequeue(Priority_1),
     {{Remainder, Priority_2, Priority_3}, Element}.
 
 
-% --- b) ---
 
-% my_create() -> // create new tree;
+tree_create() -> gb_trees:empty().
 
-trees_my_create(Priority, Elem) -> tree(Priority, [Elem]).
+tree_enqueue(Tree, Priority, Elem) ->
+    case gb_trees:is_defined(Priority, Tree) of
+        true -> 
+            Elements = gb_trees:get(Priority, Tree),
+            Updated = myqueue:enqueue(Elements, Elem),
+            gb_trees:update(Priority, Updated, Tree);
+        false ->
+            gb_trees:enter(Priority, [Elem], Tree)
+        
+        end.
 
-trees_my_enqueue(Tree, Priority, Elem) ->
-    PriorityQueue = gb_trees:get(Priority, Tree),
-    NewQueue = myqueue:enqueue(PriorityQueue, Elem),
-    gb_trees:update(Priority, NewQueue, Tree).
+% tree_dequeue(Tree) ->
+%     case myqueue:dequeue(gb_trees:get(1, Tree)) of
+%         empty ->
+%             case myqueue:dequeue(gb_trees:get(2, Tree)) of
+%                 empty ->
+%                     case myqueue:dequeue(gb_trees:get(3, Tree)) of
+%                         empty -> empty;
+%                         {Remainder, Element} ->
+%                             NewTree = gb_trees:update(3, Remainder, Tree),
+%                             {NewTree, Element}
+%                         end;
+%                 {Remainder2, Element2} ->
+%                     NewTree2 = gb_trees:update(2, Remainder2, Tree),
+%                     {NewTree2, Element2}
+%                 end;
+%         {Remainder3, Element3} ->
+%             NewTree3 = gb_trees:update(1, Remainder3, Tree),
+%             {NewTree3, Element3}
+%     end.    
 
-% Returns: {Element, UpdatedTree}
-trees_my_dequeue(Tree, Priority) -> 
-    PriorityQueue = gb_trees:get(Priority, Tree),
-    {Remainder, Element} = myqueue:dequeue(PriorityQueue),
-    {Element, gb_trees:update(Priority, Remainder, Tree)}.
+tree_dequeue(Tree) ->
+    Keys = gb_trees:keys(Tree),
+    tree_dequeue_helper(Keys, Tree).
+
+tree_dequeue_helper([], _) -> empty;
+tree_dequeue_helper([Key|Keys], Tree) ->
+    case gb_trees:get(Key, Tree) of 
+        [] ->
+            tree_dequeue_helper(Keys, Tree);
+        [Element|Remainder] ->
+            NewTree = gb_trees:update(Key, Remainder, Tree),
+            {NewTree, Element}
+        end.
