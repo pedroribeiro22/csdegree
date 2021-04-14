eea0c95be9178bcf7b5c6894b963934598a790dd
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2021-04-14 17:33:50 +0100

    [SDLE]: Guide 3

--- 4rd year/2nd_semester/SDLE/guide3/main.py
+++ 4rd year/2nd_semester/SDLE/guide3/main.py
@@ -1,5 +1,8 @@
 import networkx as nx
 import random
+import json
+import matplotlib.pyplot as plt
+
 
 class Edge:
 
@@ -39,6 +42,7 @@ class GraphAndWeights:
     def get_graph(self):
         return self.graph
 
+
 def generate_graph_and_weights(node_count):
     graph = nx.Graph()
     node_weights = {}
@@ -47,11 +51,13 @@ def generate_graph_and_weights(node_count):
         node_weights[str(i)] = 1
     return GraphAndWeights(graph, node_weights)
 
+
 def generate_weighted_random_edge(full_graph):
     origin_node = random.sample(list(full_graph.node_weights.keys()), 1)[0]
     chosen_node = random.choices(list(full_graph.node_weights.keys()), list(
         full_graph.node_weights.values()), k=1)[0]
-    return int(origin_node), int(chosen_node) 
+    return int(origin_node), int(chosen_node)
+
 
 def generate_random_attached_graph(number_of_nodes):
     graph = nx.Graph()
@@ -75,6 +81,7 @@ def generate_random_attached_graph(number_of_nodes):
             iterations = iterations + 1
     return graph, iterations
 
+
 def generate_preferential_attachment_graph(number_of_nodes):
     graph = generate_graph_and_weights(number_of_nodes)
     iterations = 0
@@ -93,6 +100,7 @@ def generate_preferential_attachment_graph(number_of_nodes):
             iterations = iterations + 1
     return graph.get_graph(), iterations
 
+
 def propagate_message(graph):
     message_source = random.sample(graph.nodes, 1)[0]
     received = set([message_source])
@@ -107,6 +115,7 @@ def propagate_message(graph):
                     messages_sent += 1
     return messages_sent
 
+
 def run_study(lower_bound, upper_bound, step, runs, random_or_preferential, messages_to_propagate):
 
     results = {}
@@ -114,7 +123,7 @@ def run_study(lower_bound, upper_bound, step, runs, random_or_preferential, mess
     nodes = range(lower_bound, upper_bound, step)
 
     for i in nodes:
-        
+
         message_count_results = []
         link_count_results = []
         diameter_results = []
@@ -126,22 +135,67 @@ def run_study(lower_bound, upper_bound, step, runs, random_or_preferential, mess
             if random_or_preferential == 1:
                 graph, edges = generate_preferential_attachment_graph(i)
             link_count_results.append(edges)
-            neighbor_count_list = [len(list(graph.neighbors(node))) for node in graph.nodes]
-            neighbor_results.append(sum(neighbor_count_list) / len(neighbor_count_list))
-            diameter_results.append(nx.algorithms.distance_measures.diameter(graph))
+            neighbor_count_list = [len(list(graph.neighbors(node)))
+                                   for node in graph.nodes]
+            neighbor_results.append(
+                sum(neighbor_count_list) / len(neighbor_count_list))
+            diameter_results.append(
+                nx.algorithms.distance_measures.diameter(graph))
             # Propagate messages
             for _ in range(messages_to_propagate):
                 msgs_sent = propagate_message(graph)
                 message_count_results.append(msgs_sent)
 
-        neighbor_count_list = [len(list(graph.neighbors(r))) for r in graph.nodes]
+        neighbor_count_list = [len(list(graph.neighbors(r)))
+                               for r in graph.nodes]
         results[i] = {
             'average_links': sum(link_count_results) / len(link_count_results),
             'average_diameter': sum(diameter_results) / len(diameter_results),
             'average_messages_sent': sum(message_count_results) / len(message_count_results),
             'average_neighbours': sum(neighbor_count_list) / len(neighbor_count_list)
         }
+    return results
+
+
+def export_results(results, lower_bound, upper_bound, step):
+    with open('results/results.json', 'w') as f:
+        json.dump(results, f)
+
+    nodes = range(lower_bound, upper_bound, step)
+    edgesArray = [n['average_links'] for n in results.values()]
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Average Number of Edges Inserted")
+    plt.axis([min(nodes), max(nodes), min(edgesArray), max(edgesArray)])
+    plt.plot(nodes, edgesArray)
+    plt.savefig('results/edges.png')
+    plt.clf()
+
+    diameterArray = [n['average_diameter'] for n in results.values()]
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Average Diameter")
+    plt.axis([min(nodes), max(nodes), min(diameterArray), max(diameterArray)])
+    plt.plot(nodes, diameterArray)
+    plt.savefig('results/diameter.png')
+    plt.clf()
+
+    neighborsArray = [n['average_neighbours'] for n in results.values()]
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Average Number of neighbors")
+    plt.axis([min(nodes), max(nodes), min(neighborsArray), max(neighborsArray)])
+    plt.plot(nodes, neighborsArray)
+    plt.savefig('results/neighbors.png')
+    plt.clf()
+
+    avgMsgsArray = [n['average_messages_sent'] for n in results.values()]
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Average Number of messages sent")
+    plt.axis([min(nodes), max(nodes), min(avgMsgsArray), max(avgMsgsArray)])
+    plt.plot(nodes, avgMsgsArray)
+    plt.savefig('results/msgs.png')
+    plt.clf()
+
 
 if __name__ == "__main__":
 
-    run_study(1, 156, 5, 40, 0, 1000) 
+    results = run_study(1, 156, 5, 40, 0, 1000)
+    export_results(results, 1, 156, 5)
